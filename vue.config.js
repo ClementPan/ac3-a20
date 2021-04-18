2369bfc6a4677b1c9dbeadfd7ad530755a8d3c0f
ClementPan <zzz2999582@gmail.com>
2021-04-18 10:11:59 +0800

    fixing vue.config.js

--- vue.config.js
+++ vue.config.js
@@ -1,7 +1,6 @@
 // ./vue.config.js
 module.exports = {
   publicPath: process.env.NODE_ENV === 'production'
-    ? '/forum-front-end-vue/'
+    ? 'ac3-a20'
     : '/'
 }
-// 如此 github page 的網址才看得到 forum-ront-end-vue
